1c60315ecd3ebcbfc4b3c51f2151dd6c7dab72e1
ritorocks <ritorocks@gmail.com>
2024-07-13 23:40:00 +0000

    Added view templates for home page and two dice 6

--- dice.rb
+++ dice.rb
@@ -8,15 +8,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get("/") do
-  "
-  <h1>Dice Roll</h1>
-  <ul>
-    <li><a href=\"/dice/2/6\">Roll two 6-sided dice</a></li>
-    <li><a href=\"/dice/2/10\">Roll two 10-sided dice</a></li>
-    <li><a href=\"/dice/1/20\">Roll one 20-sided die</a></li>
-    <li><a href=\"/dice/5/4\">Roll five 4-sided dice</a></li>
-  </ul>
-  "
+  erb(:homepage)
 end
 
 get("/zebra") do
@@ -32,10 +24,9 @@ get("/dice/2/6") do
 	second_die = rand(1..6)
   sum = first_die + second_die
 	
-	outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+  @outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
 	
-	"<h1>2d6</h1>
-   <p>#{outcome}</p>"
+	erb(:two_six)
 end
 
 get("/dice/2/10") do
